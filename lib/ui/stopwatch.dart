44689458cc12c4cd159cfd8a13fefa6f977fdac7
iAbhishekPatil <abhishek.patil@joflee.com>
2022-01-02 19:51:38 +0530

    commented

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,6 +1,7 @@
-import 'package:clock_demo/ui/elapsed_time_text_basic.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+
+import 'elapsed_time_text_basic.dart';
 
 class MyStopwatch extends StatefulWidget {
   const MyStopwatch({Key? key}) : super(key: key);
@@ -11,24 +12,30 @@ class MyStopwatch extends StatefulWidget {
 
 class _MyStopwatchState extends State<MyStopwatch>
     with SingleTickerProviderStateMixin {
-  // DATETIME STATE VARIABLE
+  // ticker object
   late final Ticker _ticker;
+
+  // elapsed variable
   Duration _elapsed = Duration.zero;
 
   @override
   void initState() {
     super.initState();
+
+    // create ticker
     _ticker = Ticker((elapsed) {
       setState(() {
         _elapsed = elapsed;
       });
     });
 
+    // start ticker
     _ticker.start();
   }
 
   @override
   void dispose() {
+    // dispose ticker
     _ticker.dispose();
     super.dispose();
   }
