32b33ca9c9e3062e273db92a5a85e63601acd80f
iAbhishekPatil <abhishek.patil@joflee.com>
2022-01-02 19:22:17 +0530

    text style added

--- lib/ui/elapsed_time_text_basic.dart
+++ lib/ui/elapsed_time_text_basic.dart
@@ -14,10 +14,14 @@ class ElapsedTimeTextBasic extends StatelessWidget {
     final int minutes = elapsed.inMinutes % 60;
 
     // DURATION STRING VARIABLES
-    final String hundredsStr = hundreds.toStringAsFixed(2).padLeft(2, '0');
+    final String hundredsStr = hundreds.toStringAsFixed(0).padLeft(2, '0');
     final String secondsStr = seconds.toString().padLeft(2, '0');
     final String minutesStr = minutes.toString().padLeft(2, '0');
 
-    return Text("$minutesStr:$secondsStr.$hundredsStr");
+    return Text(
+      "$minutesStr:$secondsStr.$hundredsStr",
+      textAlign: TextAlign.center,
+      style: const TextStyle(fontSize: 40),
+    );
   }
 }
