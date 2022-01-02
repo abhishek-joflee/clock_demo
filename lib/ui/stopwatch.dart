581dbe5ccfe5731ff6e533923ff8d853dce7bd1d
iAbhishekPatil <abhishek.patil@joflee.com>
2022-01-02 19:41:36 +0530

    ticker added

--- lib/ui/stopwatch.dart
+++ lib/ui/stopwatch.dart
@@ -1,5 +1,6 @@
 import 'package:clock_demo/ui/elapsed_time_text_basic.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class MyStopwatch extends StatefulWidget {
   const MyStopwatch({Key? key}) : super(key: key);
@@ -8,9 +9,32 @@ class MyStopwatch extends StatefulWidget {
   State<MyStopwatch> createState() => _MyStopwatchState();
 }
 
-class _MyStopwatchState extends State<MyStopwatch> {
+class _MyStopwatchState extends State<MyStopwatch>
+    with SingleTickerProviderStateMixin {
+  // DATETIME STATE VARIABLE
+  late Ticker _ticker;
+  Duration _elapsed = Duration.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    _ticker = Ticker((elapsed) {
+      setState(() {
+        _elapsed = elapsed;
+      });
+    });
+
+    _ticker.start();
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const ElapsedTimeTextBasic(elapsed: Duration(seconds: 5));
+    return ElapsedTimeTextBasic(elapsed: _elapsed);
   }
 }
