0439b46a32d90d1894b1b1f5a0351e97ada47f4c
iAbhishekPatil <abhishek.patil@joflee.com>
2022-01-02 20:45:57 +0530

    matrix4 transform

--- lib/ui/stopwatch_renderer.dart
+++ lib/ui/stopwatch_renderer.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-import 'elapsed_time_text_basic.dart';
+import 'package:flutter/material.dart';
 
 class StopwatchRenderer extends StatelessWidget {
   const StopwatchRenderer({Key? key, required this.elapsed}) : super(key: key);
@@ -11,8 +11,14 @@ class StopwatchRenderer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(color: Colors.amber),
-        ElapsedTimeTextBasic(elapsed: elapsed),
+        Transform(
+          transform: Matrix4.identity()
+            ..translate(50.0, 0.0, 0.0)
+            ..rotateZ(pi / 4),
+          alignment: Alignment.center,
+          child: Container(color: Colors.amber),
+        ),
+        // ElapsedTimeTextBasic(elapsed: elapsed),
       ],
     );
   }
